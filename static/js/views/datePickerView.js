d0c561fce76724a7e433abf84e07190edc4fff7e
David <dmrubins@gmail.com>
2014-12-14 17:21:41 -0500

    update max date

--- static/js/views/datePickerView.js
+++ static/js/views/datePickerView.js
@@ -20,7 +20,7 @@ app.datePickerView = Backbone.View.extend({
 			changeYear: true,
 			yearRange : "2014:2015",
 			minDate : new Date(),
-			maxDate : new Date(2015,6,20),
+			maxDate : new Date(2015,5,20),
 			showOtherMonths : true,
 			selectOtherMonths : true
 		});
